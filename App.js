ab87e9f761ba02257d30b92b8ef011ed7d9776a2
Rohan <rohansethi1999430@icloud.com>
2021-02-05 08:22:25 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import {createStackNavigator} from '@react-navigation/stack'
+
+
+const Stack=createStackNavigator();
+const globalScreenOptions={}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+      <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator> 
+    </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxCw1A5_Hjr67zWJFrWrSd9ZlirOkwuJA",
+    authDomain: "signal-2fc64.firebaseapp.com",
+    projectId: "signal-2fc64",
+    storageBucket: "signal-2fc64.appspot.com",
+    messagingSenderId: "397124043630",
+    appId: "1:397124043630:web:f08c63d00ef8c2ff38777f"
+  };
